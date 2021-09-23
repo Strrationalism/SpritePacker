8922d2cacd43648e4e9f643a80ea0df98b2f8bbe
Seng Jik <853974536@qq.com>
2021-09-23 17:18:46 +0800

    Bin pack

--- sppak.ml
+++ sppak.ml
@@ -1,12 +1,11 @@
-(*open Bin_pack;;*)
-
+open Bin_pack;;
 
 let help () =
     print_endline "Sprite Packer";
     print_endline "by Strrationalism Studio 2021";
     print_endline "";
     print_endline "Usage:";
-    print_endline "    sppak <inputDir> <output> [--margin <margin>]";
+    print_endline "    sppak <inputDir> <output> [--margin <margin-in-pixels>]";
     print_endline ""
 ;;
 
@@ -30,6 +29,49 @@ let get_image_files dir =
             Filename.extension file 
             |> String.lowercase_ascii in
         ext = ".png" || ext = ".bmp")
+    |> List.map (fun name ->
+        let img_result = Stb_image.load ~channels:4 (dir ^ "/" ^ name) in
+        match img_result with
+        | Ok x -> name, x
+        | Error (`Msg x) -> failwith x)
+;;
+
+
+type options = 
+    { margin: int }
+;;
+
+
+let default_options =
+    { margin = 0 }
+;;
+
+
+let rec parse_options prev_option =
+    function
+    | [] -> Ok prev_option
+    | "--margin" :: margin_value :: other -> 
+        parse_options 
+            { margin = int_of_string margin_value } 
+            other
+    | _ -> Error ()
+;;
+
+
+let process input_dir _ options =
+    if Sys.is_directory input_dir |> not then
+        failwith "<inputDir> must be a dirctory.";
+
+    match options with
+    | Error () -> help ()
+    | Ok options ->
+        get_image_files input_dir
+        |> List.map (fun (name, image) ->
+            { w = Stb_image.width image + 2 * options.margin;
+              h = Stb_image.height image + 2 * options.margin;
+              tag = name, image })
+        |> bin_pack
+        |> ignore
 ;;
 
 
@@ -37,12 +79,9 @@ let () =
     Sys.argv
     |> Array.to_list
     |> function
-        | [ _; input_dir; _ ] ->
-            if Sys.is_directory input_dir |> not then
-                failwith "<inputDir> must be a dirctory.";
-
-            get_image_files input_dir
-            |> List.iter (Printf.printf "%s\n");
+        | _ :: input_dir :: out_dir :: options ->
+            let options = parse_options default_options options in
+            process input_dir out_dir options
             
         | _ -> help ()
 ;;
